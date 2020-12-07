dba7344773866806f248418d3d017fcdc1205a71
Rishabh <mailrishabh.ra@gmail.com>
2020-12-08 01:52:15 +0530

    price table

--- grocery_item.rb
+++ grocery_item.rb
@@ -1,7 +1,7 @@
 class GroceryItem 
-  def initialize(item,price,sale_price,sale_eligible_qty) 
+  def initialize(item, unit_price, sale_price = 0, sale_eligible_qty = 0) 
     @item = item
-    @price = price
+    @unit_price = unit_price
     @sale_price = price
     @sale_eligible_qty = sale_eligible_qty
   end
---
+++ price_table.rb
@@ -0,0 +1,10 @@
+require_relative "grocery_item"
+
+module PriceTable
+  price_list = {
+      "milk" => GroceryItem.new(8.97,5,2)
+      "bread" => GroceryItem.new(2.17,6,3)
+      "apple" => GroceryItem.new(0.89)
+      "banana" => GroceryItem.new(0.99)
+  }
+end
