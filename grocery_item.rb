5a1f496347d9eeff55cd5eb1a17979e7455490f0
Rishabh <mailrishabh.ra@gmail.com>
2020-12-07 23:17:43 +0530

    grocery class

---
+++ grocery_item.rb
@@ -0,0 +1,8 @@
+class GroceryItem 
+  def initialize(item,price,sale_price,sale_eligible_qty) 
+    @item = item
+    @price = price
+    @sale_price = price
+    @sale_eligible_qty = sale_eligible_qty
+  end
+end
