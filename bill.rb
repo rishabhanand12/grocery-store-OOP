bfa927149cacdaa1631315e74454a1b2c2fc9c05
Rishabh <mailrishabh.ra@gmail.com>
2020-12-08 11:23:33 +0530

    bill class

--- bill.rb
+++ bill.rb
@@ -1,12 +1,28 @@
 require_relative price_table
 
 class Bill 
-  def initialize 
-    @bill = 0
+  attr_reader :total_amt, :saved_amt
+  
+  def initialize cart
+    @cart = cart
+    @total_amt = calculate_total_amt
+    @saved_amt = calculate_saved_amt
   end
 
-  def calculate_bill cart
-    cart.each do |item, qty|
+  def calculate_total_amt 
+    total = 0
+    @cart.each do |grocey_item|
+      total += grocey_item.amount
+    end
+    return total
   end
 
+  def calculate_saved_amt 
+    total = 0
+    price_list = PriceTable::price_list
+    @cart.each do |grocery_item| 
+      total += grocey_item.qty * price_list[grocery_item.item].unit_price
+    end
+    return total - @total_amt
+  end
 end
