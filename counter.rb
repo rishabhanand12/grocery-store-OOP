9aed451d0ebe61502e52755f0beb3c28bfb9d15e
Rishabh <mailrishabh.ra@gmail.com>
2020-12-08 12:07:46 +0530

    counter methods  added

--- counter.rb
+++ counter.rb
@@ -1,6 +1,27 @@
+require_relative "cart"
+require_relative "bill"
+
 class Counter 
-	def takeOrder
-		puts "Enter the list of grocery items seperated by a comma"
-		order_list = gets.chomp.downcase.gsub(/\s+/, '').split(',')
-	end
+  def initialize
+	@order_list = []
+	@cart = []
+	@bill = nil
+  end
+
+  def takeOrder
+    puts "Enter the list of grocery items seperated by a comma"
+    list = gets.chomp.downcase.gsub(/\s+/, '').split(',')
+    @order_list = list
+  end
+
+  def generate_cart
+	@cart = Cart.new @order_list
+  end  
+
+  def generate_bill
+	@bill != Bill.new @new_cart 
+	@bill.create_table
+	puts "\n Total Amount: $#{@bill.total_amt.round(2)}"
+	puts "\n You saved $#{@bill.saved_amt}"
+  end
 end
