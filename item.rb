bd7fd7122733af6d738a927a4fcf02bcf52c69a4
Rishabh <mailrishabh.ra@gmail.com>
2020-12-08 11:02:14 +0530

    item class

---
+++ item.rb
@@ -0,0 +1,21 @@
+require_relative "price_table"
+
+class Item 
+  def initialize(item,qty) 
+    @item = item
+    @qty = qty
+    @amount = calculate_item_amount
+  end
+
+  def calculate_item_amount
+    price_list = PriceTable::price_list
+    item_details = price_list[@item]
+    if @qty >= item_details.sale_eligible_qty
+      return ( (@qty/item_details.sale_eligible_qty) * item_details.sale_price )  +  ( (@qty%item_details.sale_eligible_qty) * item_details.unit_price )
+    else
+        @qty * item_details.unit_price
+    end
+  end
+end
+
+      
--- price_table.rb
+++ price_table.rb
@@ -2,9 +2,9 @@ require_relative "grocery_item"
 
 module PriceTable
   price_list = {
-      "milk" => GroceryItem.new(8.97,5,2)
-      "bread" => GroceryItem.new(2.17,6,3)
-      "apple" => GroceryItem.new(0.89)
-      "banana" => GroceryItem.new(0.99)
+    "milk" => GroceryItem.new(8.97,5,2)
+    "bread" => GroceryItem.new(2.17,6,3)
+    "apple" => GroceryItem.new(0.89)
+    "banana" => GroceryItem.new(0.99)
   }
 end
